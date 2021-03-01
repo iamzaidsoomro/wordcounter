25f385e82532a43a8797822dfafc810f6fc3c9f3
Zaid Ahmed <75375280+iamzaidsoomro@users.noreply.github.com>
2021-03-02 01:03:09 +0500

    Add files via upload

---
+++ wordscount.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	system("title Typing Game");
+	system("color E0");
+	char ch = 'a';
+	int word, letter;
+	cout << "Enter a phrase: ";
+	while(ch != '\r'){
+
+		ch = getche();
+		if(ch == ' '){
+			word++;
+		}else if(ch == '.' || ch == ',' || ch == '!' || ch == '\'' || ch == '\"' || ch == ':' || ch == ';' || ch == '?' || ch == '-' || ch == '(' || ch == ')' || ch == '/'){
+			letter = letter;
+		}
+		else if(ch == '\b'){
+			letter--;
+		}
+		else{
+			letter++;
+		}
+	}
+	letter -= 1;
+	cout << "\nLetters: " << letter << endl << "Words: " << word << endl;
+}
+
